c1d7912062e17392586568442bf6a9e5c73616ca
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2020-08-10 17:01:41 -0400

    Update miner.lua

--- jobs/miner.lua
+++ jobs/miner.lua
@@ -19,22 +19,22 @@ Config.Jobs.miner = {
 	Zones = {
 
 		CloakRoom = {
-			Pos = {x = 892.35, y = -2172.77, z = 31.28},
-			Size = {x = 3.0, y = 3.0, z = 1.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Pos = {x = 892.46, y = -2172.48, z = 31.32},
+			Size = {x = 2.0, y = 2.0, z = 1.0},
+			Color = {r = 0, g = 255, b = 0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_miner_locker'),
 			Type = 'cloakroom',
 			Hint = _U('cloak_change'),
-			GPS = {x = 884.86, y = -2176.51, z = 29.51}
+			GPS = 0
 		},
 
 		Mine = {
-			Pos = {x = 2962.40, y = 2746.20, z = 42.39},
+			Pos = {x = 2950.23, y = 2747.85, z = 42.51},
 			Size = {x = 5.0, y = 5.0, z = 1.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Color = {r = 0, g = 255, b = 0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_rock'),
 			Type = 'work',
@@ -52,14 +52,14 @@ Config.Jobs.miner = {
 				}
 			},
 			Hint = _U('m_pickrocks'),
-			GPS = {x = 289.24, y = 2862.90, z = 42.64}
+			GPS = 0
 		},
 
 		StoneWash = {
-			Pos = {x = 289.24, y = 2862.90, z = 42.64},
+			Pos = {x = 289.24, y = 2862.90, z = 42.68},
 			Size = {x = 5.0, y = 5.0, z = 1.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Color = {r = 0, g = 255, b = 0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_washrock'),
 			Type = 'work',
@@ -77,14 +77,14 @@ Config.Jobs.miner = {
 				}
 			},
 			Hint = _U('m_rock_button'),
-			GPS = {x = 1109.14, y = -2007.87, z = 30.01}
+			GPS = 0
 		},
 
 		Foundry = {
 			Pos = {x = 1109.14, y = -2007.87, z = 30.01},
 			Size = {x = 5.0, y = 5.0, z = 1.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Color = {r = 0, g = 255, b = 0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_rock_smelting'),
 			Type = 'work',
@@ -123,55 +123,55 @@ Config.Jobs.miner = {
 				}
 			},
 			Hint = _U('m_melt_button'),
-			GPS = {x = -169.48, y = -2659.16, z = 5.00}
+			GPS = 0
 		},
 
 		VehicleSpawner = {
-			Pos = {x = 884.86, y = -2176.51, z = 29.51},
+			Pos = {x = 887.73, y = -2175.75, z = 29.57},
 			Size = {x = 5.0, y = 5.0, z = 1.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Color = {r = 0, g = 255, b = 0},
+			Marker = 25,
 			Blip = false,
 			Name = _U('spawn_veh'),
 			Type = 'vehspawner',
 			Spawner = 1,
 			Hint = _U('spawn_veh_button'),
-			Caution = 2000,
-			GPS = {x = 2962.40, y = 2746.20, z = 42.39}
+			Caution = Config.MinerCaution,
+			GPS = 0
 		},
 
 		VehicleSpawnPoint = {
-			Pos = {x = 879.55, y = -2189.79, z = 29.51},
-			Size = {x = 5.0, y = 5.0, z = 1.0},
+			Pos = {x = 874.25, y = -2177.31, z = 30.61},
+			Size = {x = 4.5, y = 4.5, z = 1.0},
 			Marker = -1,
 			Blip = false,
 			Name = _U('service_vh'),
 			Type = 'vehspawnpt',
 			Spawner = 1,
-			Heading = 90.1,
+			Heading = 174.5,
 			GPS = 0
 		},
 
 		VehicleDeletePoint = {
-			Pos = {x = 881.93, y = -2198.01, z = 29.51},
+			Pos = {x = 873.6, y = -2198.78, z = 29.53},
 			Size = {x = 5.0, y = 5.0, z = 1.0},
 			Color = {r = 255, g = 0, b = 0},
-			Marker = 1,
+			Marker = 25,
 			Blip = false,
 			Name = _U('return_vh'),
 			Type = 'vehdelete',
 			Hint = _U('return_vh_button'),
 			Spawner = 1,
-			Caution = 2000,
+			Caution = Config.MinerCaution,
 			GPS = 0,
 			Teleport = 0
 		},
 
 		CopperDelivery = {
-			Pos = {x = -169.481, y = -2659.16, z = 5.00103},
-			Color = {r = 204, g = 204, b = 0},
-			Size = {x = 5.0, y = 5.0, z = 3.0},
-			Marker = 1,
+			Pos = {x = 854.66, y = -2113.37, z = 30.6},
+			Color = {r = 0, g = 255, b = 0},
+			Size = {x = 1.8, y = 1.8, z = 3.0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_sell_copper'),
 			Type = 'delivery',
@@ -189,15 +189,14 @@ Config.Jobs.miner = {
 				}
 			},
 			Hint = _U('m_deliver_copper'),
-			GPS = {x = -148.78, y = -1040.38, z = 26.27}
+			GPS = 0
 		},
 
 		IronDelivery = {
-			Pos = {x = -148.78, y = -1040.38, z = 26.27},
-			Color = {r = 204, g = 204, b = 0},
-			Size = {x = 5.0, y = 5.0, z = 3.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Pos = {x = 844.13, y = -2118.48, z = 29.55},
+			Color = {r = 0, g = 255, b = 0},
+			Size = {x = 1.8, y = 1.8, z = 3.0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_sell_iron'),
 			Type = 'delivery',
@@ -215,15 +214,14 @@ Config.Jobs.miner = {
 				}
 			},
 			Hint = _U('m_deliver_iron'),
-			GPS = {x = 261.48, y = 207.35, z = 109.28}
+			GPS = 0
 		},
 
 		GoldDelivery = {
-			Pos = {x = 261.48, y = 207.35, z = 109.28},
-			Color = {r = 204, g = 204, b = 0},
-			Size = {x = 5.0, y = 5.0, z = 3.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Pos = {x = 256.62, y = 219.66, z = 105.29},
+			Color = {r = 0, g = 255, b = 0},
+			Size = {x = 1.6, y = 1.6, z = 3.0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_sell_gold'),
 			Type = 'delivery',
@@ -241,15 +239,14 @@ Config.Jobs.miner = {
 				}
 			},
 			Hint = _U('m_deliver_gold'),
-			GPS = {x = -621.04, y = -228.53, z = 37.05}
+			GPS = 0
 		},
 
 		DiamondDelivery = {
-			Pos = {x = -621.04, y = -228.53, z = 37.05},
-			Color = {r = 204, g = 204, b = 0},
-			Size = {x = 5.0, y = 5.0, z = 3.0},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Pos = {x = -624.67, y = -230.99, z = 37.09},
+			Color = {r = 0, g = 255, b = 0},
+			Size = {x = 1.7, y = 1.7, z = 3.0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('m_sell_diamond'),
 			Type = 'delivery',
@@ -267,7 +264,7 @@ Config.Jobs.miner = {
 				}
 			},
 			Hint = _U('m_deliver_diamond'),
-			GPS = {x = 2962.40, y = 2746.20, z = 42.39}
+			GPS = 0
 		}
 
 	}
