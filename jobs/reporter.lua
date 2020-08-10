68da44f6f1cf89dfa1cfdd360c0e7fe42b4d3470
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2020-08-10 17:02:11 -0400

    Update reporter.lua

--- jobs/reporter.lua
+++ jobs/reporter.lua
@@ -17,18 +17,18 @@ Config.Jobs.reporter = {
 	},
 
 	Zones = {
-
+	
 		VehicleSpawner = {
-			Pos = {x = -141.41, y = -620.80, z = 167.82},
+			Pos = {x = -141.44, y = -620.96, z = 167.86},
 			Size = {x = 2.0, y = 2.0, z = 0.2},
-			Color = {r = 204, g = 204, b = 0},
-			Marker = 1,
+			Color = {r = 0, g = 255, b = 0},
+			Marker = 25,
 			Blip = true,
 			Name = _U('reporter_name'),
 			Type = 'vehspawner',
 			Spawner = 1,
 			Hint = _U('reporter_garage'),
-			Caution = 2000
+			Caution = Config.ReporterCaution
 		},
 
 		VehicleSpawnPoint = {
@@ -39,20 +39,20 @@ Config.Jobs.reporter = {
 			Name = _U('service_vh'),
 			Type = 'vehspawnpt',
 			Spawner = 1,
-			Heading = 200.1
+			Heading = 161.5
 		},
 
 		VehicleDeletePoint = {
 			Pos = {x = -144.22, y = -577.02, z = 31.42},
 			Size = {x = 5.0, y = 5.0, z = 0.2},
 			Color = {r = 255, g = 0, b = 0},
-			Marker = 1,
+			Marker = 25,
 			Blip = false,
 			Name = _U('return_vh'),
 			Type = 'vehdelete',
 			Hint = _U('return_vh_button'),
 			Spawner = 1,
-			Caution = 2000,
+			Caution = Config.ReporterCaution,
 			GPS = 0,
 			Teleport = {x = -139.09, y = -620.74, z = 167.82}
 		}
